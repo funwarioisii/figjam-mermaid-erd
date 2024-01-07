849abf46d0728bda7939dacf53989f7f773d1bca
funwarioisii <kazuyukihashimoto2006@gmail.com>
2024-01-07 19:28:40 +0900

    initial commit

---
+++ code.js
@@ -0,0 +1,114 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+figma.showUI(__html__);
+figma.ui.onmessage = (text) => __awaiter(this, void 0, void 0, function* () {
+    yield Promise.all([
+        figma.loadFontAsync({ family: "Inter", style: "Regular" }),
+        figma.loadFontAsync({ family: "Inter", style: "Medium" }),
+    ]);
+    const relations = yield parseErd(text);
+    const sortedRelations = sortRelations(relations);
+    const nodeByName = {};
+    const [startX, startY] = [
+        figma.viewport.center.x,
+        figma.viewport.center.y,
+    ];
+    sortedRelations.forEach((relation, i) => {
+        var _a, _b;
+        const fromNode = nodeByName[relation.from] || figma.createShapeWithText();
+        fromNode.text.characters = relation.from;
+        fromNode.text.fontSize = 24;
+        fromNode.shapeType = 'SQUARE';
+        fromNode.x = startX;
+        fromNode.y = startY + 200 * i;
+        nodeByName[_a = relation.from] || (nodeByName[_a] = fromNode);
+        const toNode = nodeByName[relation.to] || figma.createShapeWithText();
+        toNode.text.characters = relation.to;
+        fromNode.text.fontSize = 24;
+        toNode.shapeType = 'SQUARE';
+        toNode.x = fromNode.x - 200 * i;
+        toNode.y = fromNode.y - 200 * i;
+        nodeByName[_b = relation.to] || (nodeByName[_b] = toNode);
+        const connector = figma.createConnector();
+        connector.connectorStart = {
+            endpointNodeId: fromNode.id,
+            magnet: "AUTO",
+        };
+        connector.connectorEnd = {
+            endpointNodeId: toNode.id,
+            magnet: "AUTO",
+        };
+        connector.strokeWeight = 4;
+    });
+    const nodes = Object.keys(nodeByName).map(key => nodeByName[key]);
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+    figma.closePlugin();
+});
+/**
+ * 右から左に図を作っていくような順番に並び替える
+ *
+ * @param relations
+ * @param sortedRelations
+ * @returns
+ */
+function sortRelations(relations, sortedRelations = []) {
+    if (relations.length === 0) {
+        return sortedRelations;
+    }
+    const nextNodeName = getLastNodeName(relations);
+    const nextRelations = relations.filter(relation => relation.from !== nextNodeName);
+    const nextSortedRelations = [...sortedRelations, ...relations.filter(relation => relation.from === nextNodeName)];
+    return sortRelations(nextRelations, nextSortedRelations);
+}
+// もっとも他のノードから参照されていないノードを探す
+function getLastNodeName(relations) {
+    const nodes = relations.map(relation => relation.from);
+    nodes.push(...relations.map(relation => relation.to));
+    const nodeNames = Array.from(new Set(nodes));
+    const refferencedCountByNodeName = nodeNames
+        .reduce((acc, nodeName) => {
+        acc[nodeName] = 0;
+        return acc;
+    }, {});
+    relations.forEach(relation => {
+        refferencedCountByNodeName[relation.to]++;
+    });
+    return nodeNames.sort((a, b) => refferencedCountByNodeName[a] - refferencedCountByNodeName[b])[0];
+}
+function parseErd(erdText) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const relations = [];
+        const lines = erdText.split("\n");
+        const relationLines = lines.filter(line => line.includes("--"));
+        const RIGHT_TO_LEFT = /\|\|--[o|\|][\|{]/;
+        const LEFT_TO_RIGHT = /[\||}][o|\|]--\|\|/;
+        const LABEL_SEPARATOR = ":";
+        relationLines.forEach(relationLine => {
+            if (RIGHT_TO_LEFT.test(relationLine)) {
+                const [to, fromWithLabel] = relationLine.split(RIGHT_TO_LEFT);
+                const from = fromWithLabel.split(LABEL_SEPARATOR)[0];
+                relations.push({ from: from.trim(), to: to.trim() });
+            }
+            else if (LEFT_TO_RIGHT.test(relationLine)) {
+                const [from, toWithLabel] = relationLine.split(LEFT_TO_RIGHT);
+                const to = toWithLabel.split(LABEL_SEPARATOR)[0];
+                relations.push({ from: from.trim(), to: to.trim() });
+            }
+        });
+        return relations.reduce((acc, relation) => {
+            const { from, to } = relation;
+            if (!acc.find(r => r.from === from && r.to === to)) {
+                acc.push(relation);
+            }
+            return acc;
+        }, []);
+    });
+}
